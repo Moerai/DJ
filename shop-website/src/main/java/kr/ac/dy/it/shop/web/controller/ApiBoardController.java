8e1b7cd83e30b4899bc7d139d03e11ee76d541c2
linda7567 <linda7567@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-11-08 07:25:52 +0000

    api 게시판만들기

---
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/ApiBoardController.java
@@ -0,0 +1,123 @@
+package kr.ac.dy.it.shop.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mysql.fabric.xmlrpc.base.Member;
+
+import kr.ac.dy.it.shop.biz.model.ResponseData;
+import kr.ac.dy.it.shop.biz.service.MemberService;
+
+public class ApiBoardController {
+
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/api/member/board")
+	@ResponseBody
+	public ResponseData<Object> board(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/myboard") //내가쓴글
+	@ResponseBody
+	public ResponseData<Object> myboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	
+	@RequestMapping("/api/member/reboard") //댓글단글
+	@ResponseBody
+	public ResponseData<Object> reboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/allboard")//모든게시판
+	@ResponseBody
+	public ResponseData<Object> allboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/hotboard")//인기게시판
+	@ResponseBody
+	public ResponseData<Object> hotboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/studyboard")//공부게시판
+	@ResponseBody
+	public ResponseData<Object> studyboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/loveboard")//연애게시판
+	@ResponseBody
+	public ResponseData<Object> loveboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/drinkboard")//음주게시판
+	@ResponseBody
+	public ResponseData<Object> drinkboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/infoboard")//정보게시판
+	@ResponseBody
+	public ResponseData<Object> infoboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	@RequestMapping("/api/member/hongboboard")//홍보게시판
+	@ResponseBody
+	public ResponseData<Object> hongboboard(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		
+		
+		return null ;
+
+		
+		}
+	}
+
