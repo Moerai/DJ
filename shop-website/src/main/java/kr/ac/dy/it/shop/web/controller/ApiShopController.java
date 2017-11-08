3eacd3e79cea9abcab3450e46b7e0e06a6c3b702
linda7567 <linda7567@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-11-08 07:41:05 +0000

    api 중고장터

---
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/ApiShopController.java
@@ -0,0 +1,139 @@
+package kr.ac.dy.it.shop.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.ac.dy.it.shop.biz.dto.Member;
+import kr.ac.dy.it.shop.biz.model.ResponseData;
+import kr.ac.dy.it.shop.biz.service.MemberService;
+
+public class ApiShopController {
+
+	@Autowired
+	MemberService memberService;
+
+	@RequestMapping("/api/member/myitem")//내상품보기
+	@ResponseBody
+	public ResponseData<Object> myitem(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+		return null ;
+
+	}
+	@RequestMapping("/api/member/select")//찜목록
+	@ResponseBody
+	public ResponseData<Object> select(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+	@RequestMapping("/api/member/man")//남성의류
+	@ResponseBody
+	public ResponseData<Object> man(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+	@RequestMapping("/api/member/woman")//여성의류
+	@ResponseBody
+	public ResponseData<Object> woman(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+	@RequestMapping("/api/member/bag")//가방
+	@ResponseBody
+	public ResponseData<Object> bag(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+	@RequestMapping("/api/member/watch")//시계
+	@ResponseBody
+	public ResponseData<Object> watch(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+	@RequestMapping("/api/member/accessory")//악세사리
+	@ResponseBody
+	public ResponseData<Object> accessory(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+
+	@RequestMapping("/api/member/cosmetics")//화장품
+	@ResponseBody
+	public ResponseData<Object> cosmetics(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+
+	@RequestMapping("/api/member/perfume")//향수
+	@ResponseBody
+	public ResponseData<Object> perfume(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+		
+	}
+
+	@RequestMapping("/api/member/phone")//핸드폰
+	@ResponseBody
+	public ResponseData<Object> phone(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+
+	@RequestMapping("/api/member/elec")//전자기기
+	@ResponseBody
+	public ResponseData<Object> elec(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+
+	@RequestMapping("/api/member/book")//책
+	@ResponseBody
+	public ResponseData<Object> book(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+
+	@RequestMapping("/api/member/etc")//기타
+	@ResponseBody
+	public ResponseData<Object> etc(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+
+	@RequestMapping("/api/member/showall")//전체보기
+	@ResponseBody
+	public ResponseData<Object> showall(Model model, HttpServletRequest request, HttpServletResponse response,
+			Member parameter) {
+
+		return null ;
+
+	}
+}
