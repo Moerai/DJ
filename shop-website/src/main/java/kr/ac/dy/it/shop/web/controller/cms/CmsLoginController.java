f37adfb0cbcc4e6e8b13914a9c30f68915a55209
cider9 <cider9@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-11-22 07:44:23 +0000

    LoginController 수정 2

--- shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/cms/CmsLoginController.java
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/cms/CmsLoginController.java
@@ -1,11 +1,15 @@
 package kr.ac.dy.it.shop.web.controller.cms;
 
+import kr.ac.dy.it.shop.biz.dto.Member;
 import kr.ac.dy.it.shop.biz.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,10 +39,37 @@ public class CmsLoginController {
 
 
     //회원관리
-    @RequestMapping("/cms/member/memberList")
+    @RequestMapping("/cms/member/member-list")
     public String member(Model model, HttpServletRequest request, HttpServletResponse response) {
+    	
+    	
+    	/*List<Member> members = new ArrayList<Member>();
+    	
+    	Member member1 = new Member();
+    	member1.setUserId("id1");;
+    	member1.setUserName("홍길동");
+    	member1.setPassword("1223");    	
+    	members.add(member1);    	
+    	
+    	Member member2 = new Member();
+    	member2.setUserId("id2");;
+    	member2.setUserName("이순신");
+    	member2.setPassword("4444");    	
+    	members.add(member2);*/
+    	
+    	
+    	List<Member> members = memberService.gets();
+    	
+    	
+    	
+    	model.addAttribute("members", members);
+    	
+    	
+    	
 
         return "cms/member/memberList";
     }
+    
+    
 
 }
