0ab924c342acd76643551f9bc3b1a93b3e7213da
dddddni <dddddni@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-11-25 12:07:38 +0000

    회원가입시 정보가 db로 전송되는 컨트롤러

---
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/RegisterCheckedController.java
@@ -0,0 +1,23 @@
+package kr.ac.dy.it.shop.web.controller;
+import kr.ac.dy.it.shop.biz.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+public class RegisterCheckedController {
+
+    @Autowired
+    MemberService memberService;
+    
+    @RequestMapping("/login/registerchecked")
+    public String index(Model model, HttpServletRequest request, HttpServletResponse response) {
+    	
+        return "login/registerchecked";
+    }
+}
