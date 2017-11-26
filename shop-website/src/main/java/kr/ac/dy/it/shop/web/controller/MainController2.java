3b4cfe917bc50ee24d1a6032cba9c85d62735273
dddddni <dddddni@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-11-26 08:49:52 +0000

    메인컨트롤러

---
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/MainController2.java
@@ -0,0 +1,31 @@
+package kr.ac.dy.it.shop.web.controller;
+
+import kr.ac.dy.it.shop.biz.dto.Member;
+import kr.ac.dy.it.shop.biz.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class MainController2 {
+
+    @Autowired
+    MemberService memberService;
+
+    @RequestMapping("/login/main")
+    public String index(Model model, HttpServletRequest request, HttpServletResponse response) {
+    	
+        return "login/main";
+    }
+
+}
