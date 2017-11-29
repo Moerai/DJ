9a9d8c6b413626f3f48c74a87e4ea857dd016908
linda7567 <linda7567@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-11-29 07:32:32 +0000

    회원 DTO 수정

--- shop-website/src/main/java/kr/ac/dy/it/shop/biz/dto/Member.java
+++ shop-website/src/main/java/kr/ac/dy/it/shop/biz/dto/Member.java
@@ -9,8 +9,8 @@ import lombok.ToString;
 @EqualsAndHashCode
 
 public class Member {
-
-
+	
+	
 	String userName;
 	String state;
 	String userId;
@@ -19,76 +19,7 @@ public class Member {
 	String nickName;
 	String phone;
 	String address;
-	String student_num;
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public int getExp() {
-		return exp;
-	}
-
-	public void setExp(int exp) {
-		this.exp = exp;
-	}
-
-	public String getNickName() {
-		return nickName;
-	}
-
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	String studentNum;
 	
-	public String getStudent_num () {
-		return student_num;
-	}
-	public void setStudent_num (String student_num) {
-		this.student_num = student_num;
-	}
+	
 }
