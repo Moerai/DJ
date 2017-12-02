85fbdf4555b231922559a8372059e4d0d1cc8ac1
satcop <satcop@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-12-02 07:44:22 +0000

    가이드 소스 커밋

--- shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/cms/CmsLoginController.java
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/cms/CmsLoginController.java
@@ -3,12 +3,16 @@ package kr.ac.dy.it.shop.web.controller.cms;
 import kr.ac.dy.it.shop.biz.dto.CmsMember;
 import kr.ac.dy.it.shop.biz.service.CmsMemberService;
 import kr.ac.dy.it.shop.common.exception.FrontException;
+import kr.ac.dy.it.shop.common.util.ResponseUtils;
+import kr.ac.dy.it.shop.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -20,7 +24,7 @@ public class CmsLoginController {
 	// 로그인
 	@RequestMapping("/cms/login")
 	public String login(HttpSession session) {
-		if(session.getAttribute("id") != null)
+		if (session.getAttribute("id") != null)
 			return "redirect:/cms/member/memberList";
 		return "cms/login";
 	}
@@ -34,25 +38,31 @@ public class CmsLoginController {
 
 	// 로그인
 	@RequestMapping(path = "/cms/login", method = RequestMethod.POST)
-	public String loginSubmit(Model model, CmsMember parameter, HttpSession session) {
-		if (parameter.getId().isEmpty()) {
-			throw new FrontException("아이디를 입력해주세요.", "cms/login");
-		}else if( parameter.getPw().isEmpty()) {
-			throw new FrontException("비밀번호를 입력해주세요.","cms/login");
+	public String loginSubmit(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session
+			, CmsMember parameter) {
+
+		// 입력값 체크
+		if (StringUtils.isEmpty(parameter.getId())) {
+			ResponseUtils.htmlAlertAndHistoryback(response, "아이디를 입력해주세요.");
+		}
+		if (StringUtils.isEmpty(parameter.getPw())) {
+			ResponseUtils.htmlAlertAndHistoryback(response, "비밀번호를 입력해주세요.");
 		}
 
 		CmsMember member = cmsMemberService.selectCmsMemberById(parameter.getId());
 
 		if (member == null) {
-			throw new FrontException("아이디 혹은 비밀번호가 올바르지 않습니다.", "cms/login");
-		} else {
-			if (member.getPw().equals(parameter.getPw())) {
-				session.setAttribute("id", member.getId());
-				return "redirect:/cms/member/memberList";
-			}else {
-				throw new FrontException("아이디 혹은 비밀번호가 올바르지 않습니다.", "cms/login");
-			}
+			ResponseUtils.htmlAlertAndHistoryback(response, "이디 혹은 비밀번호가 올바르지 않습니다.");
+			return "";
 		}
+
+		if (StringUtils.equals(member.getPw(), parameter.getPw())) {
+			ResponseUtils.htmlAlertAndHistoryback(response, "아이디 혹은 비밀번호가 올바르지 않습니다.");
+			return "";
+		}
+
+		session.setAttribute("id", member.getId());
+		return "redirect:/cms/member/memberList";
 	}
 
 }
