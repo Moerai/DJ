abf24a8c05dd609a6f6cd711c774426a882dd4de
3d1935 <3d1935@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-12-06 06:15:31 +0000

    friend_list.java

---
+++ shop-website/src/main/java/kr/ac/dy/it/shop/biz/dto/Friend_list.java
@@ -0,0 +1,17 @@
+package kr.ac.dy.it.shop.biz.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@ToString
+@EqualsAndHashCode
+
+
+public class Friend_list {
+	
+	String me;
+	String friend;
+
+}
