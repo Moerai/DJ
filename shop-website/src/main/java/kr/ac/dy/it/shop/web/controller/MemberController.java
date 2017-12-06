67d03e3c3712f295f0c0b58c1a7d4409dae86f7f
dddddni <dddddni@f718f432-1d93-ee46-bce6-9aa48362f3c4>
2017-12-06 05:38:43 +0000

    수정/삭제

--- shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/MemberController.java
+++ shop-website/src/main/java/kr/ac/dy/it/shop/web/controller/MemberController.java
@@ -1,16 +1,26 @@
 package kr.ac.dy.it.shop.web.controller;
+
+import kr.ac.dy.it.shop.biz.dto.Member;
+import kr.ac.dy.it.shop.biz.model.ResponseData;
 import kr.ac.dy.it.shop.biz.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.util.List;
 
 @Slf4j
 @Controller
-public class RegisterController {
+public class MemberController {
 
     @Autowired
     MemberService registerService;
@@ -25,9 +35,17 @@ public class RegisterController {
     	
         return "member/registerchecked";
     }
-    @RequestMapping("/member/registerFinish")
+    @RequestMapping("/member/registerfinish")
     public String registerFinish(Model model, HttpServletRequest request, HttpServletResponse response) {
     	
-        return "member/registerFinish";
+        return "member/registerfinish";
     }
+    
+    @RequestMapping("/member/main")
+    public String index(Model model, HttpServletRequest request, HttpServletResponse response) {
+    	
+        return "member/main";
+    }
+
+	
 }
